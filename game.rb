e55dab505cf2b057976b2bc87617f7b81df0f5f7
jack-blomquist-nti-johanneberg <jack.blomquist@elev.ga.ntig.se>
2021-12-10 10:11:22 +0100

    update

--- game.rb
+++ game.rb
@@ -7,7 +7,7 @@ class Game < Gosu::Window
     def initialize
         @width = 1920
         @height = 1080
-        @pixel_size = 5
+        @pixel_size = 2
         
         super @width, @height, fullscreen: true
         self.caption = "Paint-spel"
@@ -22,18 +22,9 @@ class Game < Gosu::Window
 
         @color = 0xffffffff
 
-        # (0...@height/@pixel_size).each do |y|
-        #     @drawing << []
-        #     (0..@width/@pixel_size).each do |x|
-        #         @drawing[y] << 0
-        #     end
-        # end
-
-        # p @drawing
-
         @i = 0
 
-        # @drawing[10][10] = 0xffffffff
+        @text = Gosu::Image.from_text(self, "COLOR", Gosu.default_font_name, 45)
     end
  
     def update
@@ -43,15 +34,20 @@ class Game < Gosu::Window
         end
 
         if Gosu.button_down?(Gosu::KbRight)
-            @color = 0xff2f43ff
+            @color = 0xff_0000ff
         end
 
         if Gosu.button_down?(Gosu::KbUp)
-            @color = 0x00ff00ff
+            @color = 0xff_ff0000
         end
 
         if Gosu.button_down?(Gosu::KbDown)
-            @color = 0xff244f20
+            @color = 0xff_00ff00
+        end
+
+        if Gosu.button_down?(Gosu::KbZ)
+            @drawing.delete_at(-1)
+            sleep(0.1)
         end
 
         if !(mouse_x < 0 || mouse_x > @width || mouse_y < 0 || mouse_y > @height) && Gosu.button_down?(Gosu::KB_SPACE)
@@ -59,7 +55,7 @@ class Game < Gosu::Window
             @released = true
         elsif @released
             @drawing << @draw_section
-            p @drawing
+            @draw_section = Hash.new(0x0)
             @released = false
         end
 
@@ -70,16 +66,22 @@ class Game < Gosu::Window
     end
 
     def draw
-        @draw_section.each do |coord, color|
-            
-            draw_rect(coord[0] * @pixel_size, coord[1] * @pixel_size, @pixel_size, @pixel_size, Gosu::Color.new(color))
-            if @old_value.class != NilClass && (coord - @old_value).magnitude <= 150
-                draw_line(coord[0] * @pixel_size, coord[1] * @pixel_size, Gosu::Color.new(color), @old_value[0] * @pixel_size, @old_value[1] * @pixel_size, Gosu::Color.new(color))
+        @text.draw(10, 20, 0, 1, 1, Gosu::Color.new(@color))
+
+        @drawing.each do |index|
+            @old_value = nil
+            index.each do |coord, color|
+                draw_rect(coord[0] * @pixel_size, coord[1] * @pixel_size, @pixel_size, @pixel_size, Gosu::Color.new(color))
+                if @old_value.class != NilClass
+                    draw_line(coord[0] * @pixel_size, coord[1] * @pixel_size, Gosu::Color.new(color), @old_value[0] * @pixel_size, @old_value[1] * @pixel_size, Gosu::Color.new(color))
+                end
+                @old_value = coord
             end
-            @old_value = coord
+        end
+        @draw_section.each do |coord, color|
+            draw_rect(coord[0] * @pixel_size, coord[1] * @pixel_size, @pixel_size, @pixel_size, Gosu::Color.new(color))
         end
     end
-    # NY HASH SKITe
 end
 
 
